c6c216c3442775daa9f428b7db779082414534fc
TD <ims1228802@gmail.com>
2025-09-19 19:20:44 +0900

    TD : PC 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
+import Layout from "./layout/Layout.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import React, { useState } from "react";
 import MyInfo from "./pages/user/MyInfo.jsx";
 import FindEmail from "./pages/user/FindEmail.jsx";
 import FindPassword from "./pages/user/FindPassword.jsx";
-import SignUpForm from "./pages/user/SignUpForm.jsx";
+import SignUp from "./pages/user/SignUp.jsx";
 import LoginPage from "./pages/user/LoginPage.jsx";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Navigate to="/login" />} />{" "}
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/FindEmail" element={<FindEmail />} />
         <Route path="/FindPassword" element={<FindPassword />} />
-      </Routes>
+      </Routes> */}
+      <Layout>
+        <MyInfo></MyInfo>
+      </Layout>
     </div>
   );
 }
