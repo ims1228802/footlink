eaeed20a3ec81a6784a46a4b0a3966151709e62a
LeeJiseongs <137966020+LeeJiseongs@users.noreply.github.com>
2025-10-23 05:33:07 +0900

    [FL-LJS] 라우터 수정 detail : 라우터 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -47,6 +47,8 @@ import Settings from "../pages/user/Settings.jsx";
 import UserLevel from "../pages/user/UserLevel.jsx";
 import PhoneEdit from "../pages/user/PhoneEdit.jsx";
 import PasswordReset from "../pages/user/PasswordReset.jsx";
+import EditTeam from "../pages/team/EditTeam.jsx";
+import EditTeamNext from "../pages/team/EditTeamNext.jsx";
 
 
 const router = createBrowserRouter([
@@ -61,8 +63,10 @@ const router = createBrowserRouter([
   { path: "/teamList", element: <Team /> },
   { path: "/teamDetail", element: <TeamDetail /> },
   { path: "/newTeam", element: <NewTeam /> },
+  { path: "/editTeam", element: <EditTeam /> },
   { path: "/newTeamRecruit", element: <NewTeamRecruit /> },
   { path: "/newTeamNext", element: <NewTeamNext /> },
+  { path: "/editTeamNext", element: <EditTeamNext /> },
   { path: "/newTeamRecruitNext", element: <NewTeamRecruitNext /> },
   { path: "/match", element: <HomePage /> },
   { path: "/result", element: <ResultPage /> },
